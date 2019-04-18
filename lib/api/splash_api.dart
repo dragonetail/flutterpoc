f05248f8bd52274b5c4ca69d0f3a66fc1fbbb596
dragonetail <dragonetail@gmail.com>
2019-04-19 00:06:10 +0800

    基本完成了Splash guide和AD的实现，重构成BLOC架构

--- lib/api/splash_api.dart
+++ lib/api/splash_api.dart
@@ -2,19 +2,20 @@ import './http_dio.dart';
 import 'package:flutterpoc/models/index.dart';
 
 class SplashApi {
+  //Online JSON REST mock server, all data is in db.json 
   static const String splash_base_url =
-      'https://raw.githubusercontent.com/dragonetail/flutterpoc/master/lib/mockdata/splash/';
+      'https://my-json-server.typicode.com/dragonetail/flutterpoc';
 
   static Future<SplashModel> getSplashMode() async {
     {
       try {
         Response response = await dio.get(
-          splash_base_url + '/splash.json',
+          splash_base_url + '/splash',
           //queryParameters: {"id": 12, "name": "wendu"},
           options: expectHttpOK,
         );
         print(response.data);
-        return response.data as SplashModel;
+        return SplashModel.fromJson(response.data);
       } on DioError catch (error) {
          print(error);
         return null;
